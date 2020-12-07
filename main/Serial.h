4bb0d3e2d3b556db43166d69ebfc839bc17de3e1
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2020-12-07 12:46:24 -0700

    removed ambiguous conflict for print(int val) func signature creating warnings or errors in compilation process

--- main/Serial.h
+++ main/Serial.h
@@ -4,7 +4,6 @@ public:
     void begin(int val){}
     void print(const char* val){}
     void print(float val){}
-    void print(int val){}
     void print(float val1, int val2){}
     void println(){}
     void println(const char* val){}
