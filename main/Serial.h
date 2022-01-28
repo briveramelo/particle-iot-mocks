dc09bb7417cd75c4a248090a726863628677af1f
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2022-01-27 17:26:11 -0700

    added a few more overrides to Serial

--- main/Serial.h
+++ main/Serial.h
@@ -5,11 +5,14 @@ class SerialStream : public Stream
 public:
     void begin(int val){}
     void end(){}
+    void print(int val){}
     void print(float val){}
     void print(const char* val){}
     void print(float val1, int val2){}
     void println(){}
+    void println(int val){}
     void println(const char* val){}
+    void println(uint8_t val1, const unsigned char charac){}
     void println(const char* val, int num){}
     void println(const char* val, const char* other){}
 
