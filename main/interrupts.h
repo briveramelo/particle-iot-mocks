4d3068679208634d6dd87cc6f3429036c1e3af7c
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2022-02-15 17:07:41 -0700

    added override method for interrupt attachment

--- main/interrupts.h
+++ main/interrupts.h
@@ -6,3 +6,5 @@
 
 typedef std::function<void()> wiring_interrupt_handler_t;
 bool attachInterrupt(uint16_t pin, wiring_interrupt_handler_t handler, InterruptMode mode, int8_t priority = -1, uint8_t subpriority = 0);
+template <typename T>
+bool attachInterrupt(uint16_t pin, void (T::*handler)(), T *instance, InterruptMode mode, int8_t priority = -1, uint8_t subpriority = 0);
