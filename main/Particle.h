6a8d2c503ee4be9582f5aa6415b992fe471cf62f
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2023-02-06 11:32:43 -0600

    added Particle.variable function

--- main/Particle.h
+++ main/Particle.h
@@ -7,6 +7,10 @@ public:
     bool connected(){return false;}
     void connect(){}
     void syncTime(){}
+
+    template <typename T, typename... ArgsT>
+    static inline bool variable(const T &name, ArgsT&&... args){return false;}
+
     bool function(const char * funcName, int (*callback)(String extra)){return false;}
     void unsubscribe(){}
     void subscribe(const char * topic, void (*callback)(const char *topic, const char *data)){}
